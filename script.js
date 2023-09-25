8245b6e1a5c14e2a1d126337aeba42a855b07950
Andres Felipe Quevedo Vega <135791986+AndresQueved0@users.noreply.github.com>
2023-09-25 17:05:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-/*Ejercicio1
-const edadusuriario = parseInt(prompt('¿Cual es su edad?'));
-if (edadusuriario >= 18) {
-	alert('Eres mayor de edad');
+/* EJERCICIO 1
+
+let edad = 17;
+if (edad <= 18) {
+	alert('Es menor de edad');
 } else {
-	alert('Eres menor de edad');
+	alert('Es mayor de edad');
 }
-/*EJERCICIO 2 .dadas las 3 calificaciones que el estudiante obtuvo en un curso:
-A. Determinar s u promedio.
-B. Indicar si el estudiante aprobó o no aprobó el curso.
+*/
 
-Las notas deben ser de 1 a 10 y se aprueba con un promedio mayor que 6*/
+/*EJERCICIO 2
 
-/*var nota, nota, nota, promedio;
+var nota, nota, nota, promedio;
 nota = parseFloat(prompt('nota 1'));
 nota = parseFloat(prompt('nota 2'));
 nota = parseFloat(prompt('nota 3'));
@@ -23,77 +22,167 @@ if (promedio >= 6) {
 } else {
 	document.write('El promedio es ' + promedio + ' DESAPROBADO');
 }
+*/
 
-/* EJERCICIO 3 Dados los números A y B, determinar si A es mayor, menor o igual que B.*/
+/*EJERCICIO 3
 
-/*let num1 = parseFloat(prompt('Escriba un numero de 1 a 100'));
+let num1 = parseFloat(prompt('Escriba un numero de 1 a 100'));
 let num2 = parseFloat(prompt('Escriba un numero de 1 a 100'));
 
 if (num1 > num2) {
-	alert('el numero ' + num1 + ' es mayor que' + num2);
+	alert('El numero ' + num1 + ' es mayor que ' + num2);
 } else if (num1 < num2) {
-	alert('El numero ' + num1 + ' es menor que' + num2);
+	alert('El numero ' + num1 + ' es menor que ' + num2);
 } else {
 	alert('El numero ' + num1 + ' es igual que ' + num2);
 }
+*/
 
-/*EJERCICIO 4 Que lea 2 números y los imprima en forma ascendente*/
+/*EJERCICIO 4
 
-/*let numero1 = parseFloat(prompt('Ingrese el primer numero: '));
+let numero1 = parseFloat(prompt('Ingrese el primer numero: '));
 let numero2 = parseFloat(prompt('Ingrese el segundo numero: '));
 
-var ascendente = (numero1, numero2).sort(function (a, b) {
+var ascendentes = [numero1, numero2].sort(function (a, b) {
 	return a - b;
 });
-alert('Los números en forma ascendente son: ' + ascendente.join(','));
-/*EJERCICIO 5 Dado el peso de una persona:
-A. Determinar si el peso es menor o igual a 40kg. Muestre el mensaje persona baja de peso
-B. Si el peso es entre 41 y 70kg. Muestre el mensaje persona peso promedio
-C. Si el peso es mayor a 71kg. Muestra el mensaje persona con sobre  peso*/
+alert('Los números en forma ascendente son: ' + ascendentes.join(',')); 
+*/
 
-/*let peso = parseFloat(prompt('Ingrese su peso en kilogramos'));
+/*EJERCICIO 5
 
+let peso = parseFloat(prompt('Ingrese su peso en kilogramos'));
 if (peso < 40) {
 	alert('Persona baja de peso');
-} else if (peso >= 41 + peso <= 70) {
+} else if ((peso >= 40, peso <= 70)) {
 	alert('La persona tiene peso promedio');
-} else if (peso > 71) {
-	alert('La persona tiene sobrepeso');
-}*/
-/*EJERCICIO6. Dado un numero entre 1 y 7 escriba su correspondiente día de la semana si:
-Dia 1 lunes
-Dia 2 martes
-Dia 3 miercoles
-Dia 4 jueves
-Dia 5 viernes
-Dia 6 sabato
-Dia 7 domingo
-Si se escribe otro numero diferente debe salir el mensaje: ESCRIBIO UN NÚMERO FUERA DEL RANGO. ESCRIBA UN NUMERO ENTRE 1 Y 7*/
+} else alert('La persona tiene sobrepeso');
+*/
 
-/*let num=parseFloat(prompt("Ingrese un numero entre 1 y 7"));
+/*EJERCICIO 6
+
+let num=parseFloat (prompt("Ingrese un número para ver con que día corresponde"));
     switch (numero) {
-		case 1:
-			alert('Dia 1 - Lunes');
-			break;
-		case 2:
-			alert('Dia 2 - Martes');
-			break;
-		case 3:
-			alert('Dia 3 - Miercoles');
-			break;
-		case 4:
-			alert('Dia 4 - Jueves');
-			break;
-		case 5:
-			alert('Dia 5 - Viernes');
-			break;
-		case 6:
-			alert('Dia 6 - Sábado');
-			break;
-		case 7:
-			alert('Dia 7 - Domingo');
-			break;
-            default:
-            alert('ESCRIBíO UN NÚMERO FUERA DEL RANGO. ESCRIBA UN NUMERO ENTRE 1 Y 7');
+        case 1:
+            alert("Día 1 - Lunes");
             break;
-	}*/
+        case 2:
+            alert("Día 2 - Martes");
+            break;
+        case 3:
+            alert("Día 3 - Miercoles");
+            break;
+        case 4:
+            alert("Día 4 - Jueves");
+            break;
+        case 5:
+            alert("Día 5 - Viernes");
+            break;
+        case 6:
+            alert("Día 6 - Sabado");
+            break;
+        case 7:
+            alert("Día 7 - Domigo");
+            break;
+        default:
+            alert("ESCRIBIO UN NÚMERO FUERA DEL RANGO. ESCRIBA UN NÚMERO ENTRE 1 Y 7");
+    }
+*/
+
+/*EJERCICIO 7 
+
+var Total = parseFloat(prompt("Ingrese el precio total de su compra:"));
+
+var descuento;
+if (Total >=1000){
+    descuento = Total * 0.2; 
+}else{
+    descuento = Total * 0.05;
+}
+var pago = Total - descuento;
+alert ("La cantidad a pagar es: " + pago.toFixed(2) + " pesos.");
+*/
+
+/*EJERCICIO 8
+
+var horas = parseInt(prompt("Ingrese el número de horas trabajadas:"));
+
+var salario;
+if (horasTrabajadas <= 40) {
+    salario = horas * 16000;
+} else{
+    salario = horas * 20000;
+}
+    alert("Su salario semanal es de " + salario.toFixed(2) + " pesos ");
+*/
+
+/*EJERCICIO 9
+
+var nombre1 = prompt("Escriba el nombre de la primera persona:");
+var edad1 = parseInt(prompt("Escriba la edad de la primera persona"));
+
+var nombre2 = prompt("Escriba el nombre de la segunda persona:");
+var edad2 = parseInt(prompt("Escriba la edad de la segunda persona"));
+
+var mayor;
+    if (edad1 > edad2) {
+        mayor = nom1;
+    } else if (edad2 > edad1) {
+        mayor = nom2;
+    } else {
+        mayor = "Ambas personas tienen la misma edad";
+    }
+        alert("La persona mayor es: " + mayor);
+*/
+
+/*EJERCICIO 10
+
+var cantidadCamisas = parseInt(prompt("Ingrese la cantidad de camisas compradas:"));
+var precioCamisa = parseFloat(prompt("Ingrese el precio de cada camisa:"));
+var TotalSinDescuento = cantidadCamisas * precioCamisa;
+var descuento; 
+    if (cantidadCamisas >=3) {
+        descuento = TotalSinDescuento * 0.2;
+    } else {
+        descuento = TotalSinDescuento * 0.1;
+    }
+    var TotalConDescuento = TotalSinDescuento - descuento;
+        alert("El total a pagar por la compra de camisas es: " + TotalConDescuento.toFixed(2) + "pesos.");
+*/
+
+/*EJERCICIO 11
+
+var num1 = parseFloat(prompt("Escriba el primer número:"));
+var num2 = parseFloat(prompt("Escriba el segundo número:"));
+    if (num1 === num2) {
+        var resultado = num1 * num2;
+            alert("El resultado de la multiplicación es: " + resultado);
+    } else if (num1 > num2) {
+        var resultado = num1 - num2;
+            alert("El resultado de la resta es: " + resultado);
+    } else {
+        var resultado = num1 + num2;
+            alert("El resultado de la suma es: " + resultado);
+    }
+*/
+
+/*EJERCICIO 12
+
+let opcion=prompt("1.suma 2.resta 3.división 4.multiplicación");
+let num1=prompt("Digite el primer número");
+let num2=prompt("Digite el segundo número");
+
+    if(opcion==1){
+        suma=parseInt(num1) + parseInt(num2);
+            alert("La suma es: "+suma);
+    }else if(opcion==2){
+        resta=parseInt(num1) - parseInt(num2);
+            alert("La resta es: "+resta);
+    }else if(opcion==3){
+        división=parseInt(num1) / parseInt(num2);
+            alert("La división es: "+división);
+    }else {
+        multiplicación=parseInt(num1) * parseInt(num2);
+            alert("La multiplicación es: "+multiplicación);
+    }
+*/
